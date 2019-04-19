063bf6ec5eb44501054b1cd50bff4dad3f4086b8
bhawna <bhawnabudhiraja1993@gmail.com>
2019-04-19 16:38:52 +0530

    Initial Push

---
+++ src/main/java/com/io/controller/ControllerDemo.java
@@ -0,0 +1,13 @@
+package com.io.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerDemo {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ControllerDemo.class, args);
+	}
+}
